d8f97b911f8dbc9d1e982ace6babf6f1aa0d5df8
kmeurer <kevin.a.meurer@gmail.com>
2014-09-10 16:29:08 -0700

    Finished protypal function for stack. yay.

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,27 @@
 var makeStack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var myStack = Object.create(stackMethods);
+  myStack.length = 0;
+  return myStack;
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(value){
+  this[this.length] = value;
+  this.length++;
+};
+
+stackMethods.pop = function() {
+  if(this.length !== 0){
+    var removed = this[this.length-1];
+    delete this[this.length-1];
+    this.length--;
+    return removed;
+  }
+};
+
+stackMethods.size = function(){
+  return this.length;
+};
 
 

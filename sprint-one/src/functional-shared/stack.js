925b684caca086518d6598923846120077280184
kmeurer <kevin.a.meurer@gmail.com>
2014-09-10 15:54:08 -0700

    finished first functional shared(stack)

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,32 @@
 var makeStack = function() {
+  var myStack = {};
+  myStack.length = 0;
+  _.extend(myStack, stackMethods);
+  return myStack;
+  //use extend to apply methods
+
+
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value){
+  this[this.length] = value;
+  this.length++;
+};
+
+stackMethods.pop = function(){
+  if(this.length !== 0){
+    var removed = this[this.length - 1];
+    delete this[this.length - 1];
+    this.length--;
+    return removed;
+  }
+}
+
+stackMethods.size = function(){
+  return this.length;
+};
 

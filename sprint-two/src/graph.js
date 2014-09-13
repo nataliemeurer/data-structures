51a557e2b70cef19180b2b10a5b69f8a09480d05
Kevin Meurer <kevin.a.meurer@gmail.com>
2014-09-12 19:38:56 -0700

    started graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,13 +1,41 @@
 var Graph = function(){
+	this.nodes = {};
+	this.size = 0;
 };
 
 Graph.prototype.addNode = function(newNode, toNode){
+  // check size // 0
+  // check toNode
+
+  // make a node out of newNode
+  var node = { edges: [] };
+
+  // add a newNode key to our nodes
+  this.nodes[newNode] = node;
+
+  // increment size
+  this.size++;
 };
 
 Graph.prototype.contains = function(node){
+  // check graph for passed in value
+  // if it exists
+  if (this.nodes[node] !== undefined) {
+    return true;
+  // if not return false
+  } else {
+    return false;
+  }
 };
 
 Graph.prototype.removeNode = function(node){
+  // check if that node actually exists
+  // if it does 
+    // delete it
+    // decrement size
+    // return it
+  // otherwise
+    // null
 };
 
 Graph.prototype.getEdge = function(fromNode, toNode){
